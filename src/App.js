27a2b438615e07b8ed11ae28b5277acaf831f581
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-05-21 03:09:47 +0700

    them trang search va xay dung UI input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
-import DetailMoviePage from "./pages/DetailMoviePage";
-import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import Topbar from "./components/Topbar";
+import DetailMoviePage from "./pages/DetailMoviePage";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Switch>
             <Route exact path={"/home"} component={HomePage} />
             <Route exact path={"/movie/:movieID"} component={DetailMoviePage} />
+            <Route exact path={"/search"} component={SearchPage} />
             <Route exact path={"/"} render={() => <Redirect to="/home" />} />
           </Switch>
         </div>
---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class SearchPage extends Component {
+  componentDidMount() {
+    this.props.dispatch({
+      type: "HIDE_NAVBAR",
+    });
+  }
+
+  render() {
+    return (
+      <div className="search-page">
+        <div className="sp-container">
+          <div className="ui action input sp-search">
+            <input type="text" placeholder="Search..." className="sp-input" id="sp-input" />
+            <select className="ui compact selection dropdown sp-select">
+              <option value="all">All</option>
+              <option value="movies">Movies</option>
+              <option value="people">People</option>
+            </select>
+            <div className="ui black button sp-button">Search</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect()(SearchPage);
