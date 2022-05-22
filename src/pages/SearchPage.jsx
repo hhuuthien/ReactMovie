894adb39cf287698acac5d9576affc60cb9ee0a0
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-05-22 23:36:28 +0700

    execute search API

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { API_KEY, LANGUAGE, PREFIX } from "../data/configData";
 
 class SearchPage extends Component {
   componentDidMount() {
@@ -8,12 +10,42 @@ class SearchPage extends Component {
     });
   }
 
+  state = {
+    keyword: "",
+  };
+
+  handleChange = (event) => {
+    let { value } = event.target;
+    this.setState(
+      {
+        keyword: value,
+      },
+      async () => {
+        const url = `${PREFIX}/search/movie?api_key=${API_KEY}&language=${LANGUAGE}&page=1&include_adult=true&query=${this.state.keyword}`;
+        try {
+          const { data } = await axios.get(url);
+          console.log(data);
+        } catch (error) {
+          console.log(error.response.data);
+        }
+      }
+    );
+  };
+
   render() {
     return (
       <div className="search-page">
         <div className="sp-container">
           <div className="ui action input sp-search">
-            <input type="text" placeholder="Search..." className="sp-input" id="sp-input" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="sp-input"
+              id="sp-input"
+              onChange={(event) => {
+                this.handleChange(event);
+              }}
+            />
             <br />
             <select className="ui compact selection dropdown sp-select">
               <option value="all">All</option>
