7fdc90a06f25adf5443d85afa8e1c05af12262c3
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-05-27 12:57:28 +0700

    Reboot dự án với giao diện mới

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Topbar from "./components/Topbar";
-import DetailMoviePage from "./pages/DetailMoviePage";
-import DetailPeoplePage from "./pages/DetailPeoplePage";
-import HomePage from "./pages/HomePage";
-import MovieImageGallery from "./pages/MovieImageGalleryPage";
-import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Topbar />
-        <div className="App-main">
-          <Navbar />
-          <Switch>
-            <Route exact path={"/home"} component={HomePage} />
-            <Route exact path={"/movie/:movieID"} component={DetailMoviePage} />
-            <Route exact path={"/people/:peopleID"} component={DetailPeoplePage} />
-            <Route exact path={"/search"} component={SearchPage} />
-            <Route exact path={"/image/:type/:imageIndex"} component={MovieImageGallery} />
-            <Route exact path={"/"} render={() => <Redirect to="/home" />} />
-          </Switch>
-        </div>
-      </div>
+      <div className="app">Tái khởi động dự án</div>
     </BrowserRouter>
   );
 }
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,17 +1,7 @@
 import { combineReducers, createStore } from "redux";
-import { movieReducer } from "./reducer/movieReducer";
-import { navbarReducer } from "./reducer/navbarReducer";
-import { modalReducer } from "./reducer/modalReducer";
-import { searchReducer } from "./reducer/searchReducer";
-import { peopleReducer } from "./reducer/peopleReducer";
 
 const rootReducer = combineReducers({
   // reducer here
-  movieReducer,
-  navbarReducer,
-  modalReducer,
-  searchReducer,
-  peopleReducer,
 });
 
 export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
