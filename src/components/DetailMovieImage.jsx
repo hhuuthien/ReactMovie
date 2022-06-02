ede75eed876c8fe0c640c5de754ffb104a3630d0
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-06-03 01:10:55 +0700

    Thiết kế giao diện gallery trình chiếu hình ảnh

--- src/components/DetailMovieImage.jsx
+++ src/components/DetailMovieImage.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
-import { Button, Header } from "semantic-ui-react";
-import { IMG_500_PREFIX } from "../data/configData";
+import { Button, Header, Modal } from "semantic-ui-react";
+import { IMG_500_PREFIX, IMG_PREFIX } from "../data/configData";
 import CustomImage from "./CustomImage";
 
 export default class DetailMovieImage extends Component {
+  state = {
+    isModal: false,
+    img: "",
+  };
+
   render() {
     const { movieImage } = this.props;
 
@@ -19,12 +24,32 @@ export default class DetailMovieImage extends Component {
         <div className="dmimage-main-list">
           {movieImage.posters.map((p, i) => {
             return (
-              <div key={i} className="img-container">
+              <div
+                key={i}
+                className="img-container"
+                onClick={() => {
+                  this.setState({ isModal: true, img: p.file_path });
+                }}
+              >
                 <CustomImage prefix={IMG_500_PREFIX} sublink={p.file_path} placeholderSrc="/img/placeholder.png" />
               </div>
             );
           })}
         </div>
+        <Modal
+          className="dmimage-main-modal"
+          dimmer={"blurring"}
+          basic
+          onClose={() => this.setState({ isModal: false })}
+          onOpen={() => this.setState({ isModal: true })}
+          open={this.state.isModal}
+        >
+          <Modal.Content>
+            <i className="fa-solid fa-caret-left prev"></i>
+            <CustomImage prefix={IMG_PREFIX} sublink={this.state.img} placeholderSrc="/img/placeholder.png" />
+            <i className="fa-solid fa-caret-right next"></i>
+          </Modal.Content>
+        </Modal>
       </div>
     );
   }
